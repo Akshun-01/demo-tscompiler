8a839b2d9ba9f6d5b179c8c4b22b02c0a21d8039
Akshun-01 <akshunkuthiala2002@gmail.com>
2024-06-15 14:14:53 +0530

    getting results: needs recurssion

--- src/scripts/compiler.ts
+++ src/scripts/compiler.ts
@@ -1,51 +1,52 @@
 import * as path from "path";
-import * as fs from "fs";
-import * as ts from "typescript";
+import * as ts from 'typescript';
 
-function isDefined<T>(x: T | undefined): x is T {
-  return typeof x !== "undefined";
+const fileName = path.join(__dirname, '../types/room.ts');
+
+// Read the file content
+const fileContent = ts.sys.readFile(fileName);
+
+if (!fileContent) {
+    console.error(`Could not read file: ${fileName}`);
+    process.exit(1);
 }
 
-//create program
-const program = ts.createProgram({
-  options: {
-    module: ts.ModuleKind.CommonJS,
-    target: ts.ScriptTarget.ESNext,
-  },
-  rootNames: [
-    //path to ../types/room.ts
-    path.join(__dirname, "../types/room.ts"),
-  ],
-});
+// Create a SourceFile object
+const sourceFile = ts.createSourceFile(fileName, fileContent, ts.ScriptTarget.Latest, true);
 
-// get files
-let files = program
-  .getSourceFiles()
-  .filter((sf) => !sf.isDeclarationFile);
+// Function to extract endpoints from the AST
+const extractEndpoints = (node: ts.Node, endpoints: any = {}) => {
+    if (ts.isTypeAliasDeclaration(node) && node.name.text === 'RoomsEndpoints') {
+        const typeLiteral = node.type as ts.TypeLiteralNode;
+        typeLiteral.members.forEach(member => {
+            if (ts.isPropertySignature(member) && member.name && ts.isStringLiteral(member.name)) {
+                const key = member.name.text;
+                const methodType = member.type as ts.TypeLiteralNode;
 
-files = [...files].filter( f => f.fileName.includes("types"));
+                endpoints[key] = {};
 
-files.map(file => {
-  console.log(file);
-})
+                methodType.members.forEach(methodMember => {
+                    if (ts.isPropertySignature(methodMember) && methodMember.name && ts.isIdentifier(methodMember.name)) {
+                        const methodName = methodMember.name.text;
+                        const method = methodMember.type as ts.FunctionTypeNode;
 
-// get checker and symbol
-const checker = program.getTypeChecker();
+                        const params = method.parameters.map(param => param.name.getText());
+                        const returnType = method.type?.getText() ?? 'void';
 
-const sfSymbols = files
-  .map(f => checker.getSymbolAtLocation(f))
-  .filter(isDefined);
-  
+                        endpoints[key][methodName] = {
+                            params,
+                            returnType
+                        };
+                    }
+                });
+            }
+        });
+    }
 
-// for each source file symbol
-sfSymbols.forEach(sfSymbol => {
-  // console.log(sfSymbol);
-  
-  // sfSymbol.exports?.forEach(ex => {
-  //   if(ex.escapedName==="RoomsEndpoints"){
-  //     console.log(ex.declarations[0].type);
-  //   }
-  // });
-  
-})
+    ts.forEachChild(node, childNode => extractEndpoints(childNode, endpoints));
+};
 
+const endpoints = {};
+extractEndpoints(sourceFile, endpoints);
+
+console.log(JSON.stringify(endpoints, null, 2));
